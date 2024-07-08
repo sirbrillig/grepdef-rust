06d05ac46ba2034557be35f600309c29acb4e78e
Payton Swick <payton@foolord.com>
2024-07-08 13:59:55 -0400

    Add tests for properties and objects to JS test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -19,7 +19,14 @@ fn search_returns_matching_js_function_line() {
 }
 
 #[rstest]
+#[case(String::from("queryDb"), String::from("js"), 1)]
+#[case(String::from("makeQuery"), String::from("js"), 4)]
 #[case(String::from("parseQuery"), String::from("js"), 7)]
+#[case(String::from("objectWithFunctionShorthand"), String::from("js"), 15)]
+#[case(String::from("shorthandFunction"), String::from("js"), 16)]
+#[case(String::from("longhandFunction"), String::from("js"), 25)]
+#[case(String::from("longhandArrowFunction"), String::from("js"), 34)]
+#[case(String::from("longhandProperty"), String::from("js"), 43)]
 fn search_returns_expected_line_number(
     #[case] query: String,
     #[case] file_type: String,
@@ -29,6 +36,7 @@ fn search_returns_expected_line_number(
     let config_args = [String::from("test run"), query, file_path, file_type].into_iter();
     let config = Config::build(config_args).expect("Incorrect config for test");
     let actual = search(config).expect("Search failed for test");
+    assert_eq!(1, actual.len());
     let first_actual = actual.get(0).expect("Search failed for test");
     assert_eq!(line_number, first_actual.line_number);
 }

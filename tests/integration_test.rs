b22e773fa7ac54a93d421ca900f28a221753250b
Payton Swick <payton@foolord.com>
2024-07-08 13:27:36 -0400

    Add case-based test for js searches

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -6,13 +6,29 @@ fn search_returns_matching_js_function_line() {
     let file_path = String::from("./tests/js-fixture.js");
     let query = String::from("parseQuery");
     let line_number = 7;
+    let file_type = String::from("js");
     let expected = vec![SearchResult {
         file_path: file_path.clone(),
         line_number,
         text: String::from("function parseQuery() {"),
     }];
-    let config_args = [String::from("test run"), query, file_path].into_iter();
+    let config_args = [String::from("test run"), query, file_path, file_type].into_iter();
     let config = Config::build(config_args).expect("Incorrect config for test");
     let actual = search(config).expect("Search failed for test");
-    assert_eq!(expected, actual)
+    assert_eq!(expected, actual);
+}
+
+#[rstest]
+#[case(String::from("parseQuery"), String::from("js"), 7)]
+fn search_returns_expected_line_number(
+    #[case] query: String,
+    #[case] file_type: String,
+    #[case] line_number: usize,
+) {
+    let file_path = String::from("./tests/js-fixture.js");
+    let config_args = [String::from("test run"), query, file_path, file_type].into_iter();
+    let config = Config::build(config_args).expect("Incorrect config for test");
+    let actual = search(config).expect("Search failed for test");
+    let first_actual = actual.get(0).expect("Search failed for test");
+    assert_eq!(line_number, first_actual.line_number);
 }

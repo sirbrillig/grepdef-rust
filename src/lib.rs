ab8a1c49e512c5480804467e21428fc57b28fde7
Payton Swick <payton@foolord.com>
2024-07-16 13:44:41 -0400

    Refactor to run search_file in thread pool

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@ use regex::Regex;
 use std::error::Error;
 use std::fs;
 use std::io::{self, BufRead, Read, Seek};
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use strum_macros::Display;
 use strum_macros::EnumString;
@@ -123,11 +126,93 @@ fn get_regexp_for_query(query: &str, file_type: &FileType) -> Regex {
     Regex::new(regexp_string).unwrap()
 }
 
+type WorkerReceiver = Arc<Mutex<mpsc::Receiver<Job>>>;
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: WorkerReceiver) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv();
+
+            match message {
+                Ok(job) => {
+                    job();
+                }
+                Err(_) => {
+                    break;
+                }
+            }
+        });
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
+struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool
+    ///
+    /// The count is the number of threads availalable in the pool.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the size is 0.
+    pub fn new(count: usize) -> ThreadPool {
+        assert!(count > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(count);
+
+        for id in 0..count {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
 pub fn search(config: &Config) -> Result<Vec<SearchResult>, Box<dyn Error>> {
     let re = get_regexp_for_query(&config.query, &config.file_type);
     let file_type_re = get_regexp_for_file_type(&config.file_type);
+    let pool = ThreadPool::new(4);
     let mut results = vec![];
-    let mut children = vec![];
 
     for entry in Walk::new(&config.file_path) {
         let path = entry?.into_path();
@@ -145,14 +230,16 @@ pub fn search(config: &Config) -> Result<Vec<SearchResult>, Box<dyn Error>> {
         let re1 = re.clone();
         let path1 = path.clone();
         let config1 = config.clone();
-        children.push(thread::spawn(move || search_file(&re1, &path1, &config1)))
+        pool.execute(move || {
+            search_file(&re1, &path1, &config1);
+        })
     }
 
-    for child in children {
-        let result = child.join();
-        let search_result = result.unwrap_or(vec![]);
-        results.extend(search_result);
-    }
+    // for child in children {
+    //     let result = child.join();
+    //     let search_result = result.unwrap_or(vec![]);
+    //     results.extend(search_result);
+    // }
 
     Ok(results)
 }
